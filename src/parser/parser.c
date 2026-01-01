27fe5717242e943b56c928ffeae51ea600090f8d
Raghav Sharma <code.practice17@gmail.com>
2026-01-01 16:02:54 +0530

    Parser: Pipeline parsing done

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,17 +2,82 @@
 
 #include "parser.h"
 #include "ast.h"
+#include "command.h"
 #include "parser_helper.h"
 #include "pipeline.h"
 #include "token.h"
 
 
 
-static Ast_node *parse_list(Token *tokens, int *current, 
-                            Pipeline_table *pipe_table);
+static Command_obj *parse_command(Token *tokens, int *current);
+
+static Ast_node *parse_pipeline(Token *tokens, int *current,
+                                Pipeline_table *pipe_table);
 
 static Ast_node *parse_condition(Token *tokens, int *current,
                                 Pipeline_table *pipe_table);
+
+static Ast_node *parse_list(Token *tokens, int *current, 
+                            Pipeline_table *pipe_table);
+
+
+
+static Ast_node *
+parse_pipeline(Token *tokens, int *current, Pipeline_table *pipe_table)
+{
+    /*
+        Add command objs to pipeline obj and finally
+        add pipeline obj to pipeline table 
+    */
+    Pipeline_obj *pipe_obj = get_pipeline_obj();
+    if (pipe_obj == NULL) {
+        return NULL;
+    }
+
+    Command_obj *cmd_obj = parse_command(tokens, current);
+    if (cmd_obj == NULL) {
+        destroy_pipeline_obj(pipe_obj);
+        return NULL;
+    }
+
+    if (add_cmd_to_pipeline(pipe_obj, cmd_obj) == -1) {
+        destroy_pipeline_obj(pipe_obj);
+        destroy_cmd_obj(cmd_obj);
+        return NULL;
+    }
+
+    while (tokens[*current].type == PIPE) {
+        *current += 1;
+
+        cmd_obj = parse_command(tokens, current);
+        if (cmd_obj == NULL) {
+            destroy_pipeline_obj(pipe_obj);
+            return NULL;
+        }
+
+        if (add_cmd_to_pipeline(pipe_obj, cmd_obj) == -1) {
+            destroy_pipeline_obj(pipe_obj);
+            destroy_cmd_obj(cmd_obj);
+            return NULL;
+        }
+    }
+
+    int pipeline_index = add_pipeline_to_table(pipe_table, pipe_obj);
+    if (pipeline_index == -1) {
+        destroy_pipeline_obj(pipe_obj);
+        return NULL;
+    }
+
+    /* Pipeline node with index being the index 
+       in pipeline table where pipeline is stored*/
+    Ast_node *node = create_ast_node(PIPELINE, pipeline_index);
+    if (node == NULL) {
+        /* Caller will destroy pipeline table */
+        return NULL;
+    }
+
+    return node;
+}
 
 
 
@@ -31,7 +96,7 @@ parse_condition(Token *tokens, int *current, Pipeline_table *pipe_table)
     while (tokens[*current].type == DOUBLE_AMPRSND
         || tokens[*current].type == DOUBLE_PIPE) {
 
-        Node_type root_type = get_node_type(tokens, current);
+        Node_type root_type = get_node_type(tokens[*current].type);
         *current += 1;
 
         Ast_node *root = create_ast_node(root_type, -1);
@@ -75,7 +140,7 @@ parse_list(Token *tokens, int *current, Pipeline_table *pipe_table)
     while (tokens[*current].type == SEMICLN
         || tokens[*current].type == AMPRSND) {
 
-        Node_type root_type = get_node_type(tokens, current);
+        Node_type root_type = get_node_type(tokens[*current].type);
         *current += 1;
 
         Ast_node *root = create_ast_node(root_type, -1);
