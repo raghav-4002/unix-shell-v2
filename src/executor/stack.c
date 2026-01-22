a2a17b9b800d0f4bfff771c4fc91e0fe9291ff3b
Raghav Sharma <code.practice17@gmail.com>
2026-01-22 13:48:18 +0530

    Add stack

---
+++ src/executor/stack.c
@@ -0,0 +1,36 @@
+#include "stack.h"
+#include "ast.h"
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int
+push_node_into_stack(Ast_node *node, Stack **stack)
+{
+    Stack *new = malloc(sizeof(*new));
+    if (new == NULL) {
+        perror("push_node_into_stack");
+        return -1;
+    }
+
+    new->prev = *stack;
+    new->node = node;
+    *stack    = new;
+    return 0;
+}
+
+
+Ast_node *
+pop_node_from_stack(Stack **stack)
+{
+    assert(stack != NULL);
+    Ast_node *ast_node = (*stack)->node;
+    Stack *temp = *stack;
+
+    *stack = (*stack)->prev;
+    free(temp);
+
+    return ast_node;
+}
---
+++ src/executor/stack.h
@@ -0,0 +1,19 @@
+#ifndef STACK_H_
+#define STACK_H_
+
+
+#include "ast.h"
+
+
+typedef struct Stack
+{
+    Ast_node *node;
+    struct Stack *prev;
+} Stack;
+
+
+int push_node_into_stack(Ast_node *node, Stack **stack);
+Ast_node *pop_node_from_stack(Stack **stack);
+
+
+#endif // STACK_H_
